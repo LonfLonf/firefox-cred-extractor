a4f1f487e51b6df719a2e8a27da9013a15e4b545
Yuri Oliveira <yuri_souzaln@outlook.com>
2025-05-01 01:23:19 -0300

    Clear

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.RegularExpressions;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        string Path = $"C:\\Users\\{Environment.UserName}\\AppData\\Roaming\\Mozilla\\Firefox\\Profiles";
+        string Pattern = @"release";
+        var Directories = Directory.GetDirectories(Path);
+
+        RegexOptions RegexOptions = RegexOptions.Multiline;
+
+        foreach(string Folder in Directories)
+        {
+            var Matches = Regex.Matches(Folder, Pattern, RegexOptions);
+
+            foreach(Match match in Matches)
+            {
+                Console.WriteLine($"'{match.Value}' found at index {match.Index}.");
+                Console.WriteLine(Folder);
+            }
+        }
+
+
+    }
+}
